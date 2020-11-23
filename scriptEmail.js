c912e5fa2fa5b3f3ae07bfd94489831e671816ca
EetuTuominen <eetu.tuominen@edu.salpaus.fi>
2020-11-23 08:57:28 +0000

    2020.23.11

---
+++ scriptEmail.js
@@ -0,0 +1,30 @@
+
+const emailnappi = document.querySelector('.lahetanappi');
+
+emailnappi.addEventListener('click' e => {
+e.preventDefault();
+sendJSON();
+
+}};
+
+function sendJSON(){
+  let xhr = new XMLHttpRequest();
+  let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp1?code=lWOELqiU07AqsBviOQYzuNIrQP7xoV7NV7C5W2ctgjIRcf7nXE2biw==";
+
+  xhr.open("POST", url, true);
+
+  xhr.setRequestHeader("Content-type", "application/json");
+
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200){
+      console.log("valmis, yhteys toimii");
+    }
+  };
+  var data = JSON.stringify({
+    "EmailMsg": "Tähän tulee postin sisältö", Kirjoittaa sahkopostin sisallon
+    "EmailAddress": "mira.vorna@salpaus.fi", viestin kirjoittajan sahkopostin
+    "EmailTo": "eetu.tuominen@edu.salpaus.fi", sahkoposti
+    "EmailName": "Teppo tyyppi" Nimi kentan sitalto
+  })
+
+}
